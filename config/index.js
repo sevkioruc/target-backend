6fec2249baa6ba9dae701936332d57bb72b8900e
sevkioruc <sevkioruc34@gmail.com>
2022-06-14 01:12:58 +0300

    Dockerized services were added for Node and Mongo Db

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-	MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING,
+	MONGODB_URI: process.env.MONGODB_URI,
+	DB_NAME: process.env.DB_NAME,
 	JWT_SECRET: process.env.JWT_SECRET,
 	COOKIE_SEESION_KEY_1: process.env.COOKIE_SEESION_KEY_1,
 	COOKIE_SEESION_KEY_2: process.env.COOKIE_SEESION_KEY_2,
